602ec542682b107b93fbd2b6a615e209b01e5013
Agricio <agricio.vitoretti@gmail.com>
2026-03-27 15:08:09 -0300

    feat: adição da api com o backend, algumas adição do frontend e adição do readme

---
+++ README.md
@@ -0,0 +1,1 @@
+Projeto para aprender React, typescript, vite e express
--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,16 @@ app.get("/", (req, res) => {
     res.send("Backend da dashboard está rodando");
 });
 
+app.get("/api/games", (req,res) => {
+    const games =[
+        { id: 1, name: "God of War", genre: "Ação", plataform: "Playstation" },
+        { id: 2, name: "Elden ring", genre: "RPG", plataform: "PC" },
+        { id: 3, name: "Forza horizon 5", genre: "Corrida", plataform: "Xbox" },
+    ];
+
+    res.json(games);
+});
+
 app.listen(PORT, () => {
     console.log('Servidor rodando em http://localhost:${PORT}');
 });
