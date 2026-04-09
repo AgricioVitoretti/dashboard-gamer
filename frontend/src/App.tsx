093db1a498a2594af2c986e26dd9048059810b75
Agricio <agricio.vitoretti@gmail.com>
2026-04-08 21:22:11 -0300

    feat: adição de mensagem de erro do servidor

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,23 @@
  import { useEffect, useState } from "react";
  import GameCard from "./components/GameCard";
  import "./App.css";
-
-type Game = {
-  id: number;
-  name: string;
-  genre: string;
-  platform: string;
-};
+ import type { Game } from "./types/Game";
 
  function App() {
   const [games, setGames] = useState<Game[]>([]);
-
+  const [loading, setLoanding] = useState(true);
   useEffect(() => {
     fetch("http://localhost:3000/api/games")
       .then((response) => response.json())
       .then((data) => {
         setGames(data);
+        setLoanding(false);
       });
   }, []);
+
+  if (loading) {
+    return <p>Carregando jogos...</p>;
+  }
 
   return (
     <div>
